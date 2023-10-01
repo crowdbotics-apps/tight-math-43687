7899d833a9f41c5abb2143d6d97cd07ed521b956
Brendan Alwell <Alwellbrendan@gmail.com>
2023-10-01 10:06:03 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -7,11 +7,11 @@ const WelcomeScreen = () => {
         <View style={styles.group} />
         <View style={styles.group}>
           <Image style={styles.logo} source={{
-          uri: "https://th.bing.com/th/id/OIP.uAgvklrtV5dqSRI3l17FtgHaFD?w=283&h=193&c=7&r=0&o=5&pid=1.7"
+          uri: "https://th.bing.com/th/id/R.8ca146943c1fd446af8ea58a8e3685a3?rik=nAmFs30Gx7cnJw&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fncE%2fRGp%2fncERGp67i.png&ehk=tvstdro5R5UZ1nKezna51UST44u95fzC07q89n9Hsxc%3d&risl=&pid=ImgRaw&r=0"
         }} />
-          <Text style={styles.text}>{"Life saving information to the right people at the right time"}</Text>
+          <Text style={styles.text}>{"When seconds matter, pictures speak faster than words"}</Text>
         </View>
-        <Text style={styles.footer}>{"Pictures speak a thousand words"}</Text>
+        <Text style={styles.footer}>{"Safety for the 21st century"}</Text>
       </ScrollView>
     </SafeAreaView>;
 };
@@ -41,13 +41,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 28,
     color: "#828AB0",
-    fontWeight: 700
+    fontWeight: "700"
   },
   footer: {
     textAlign: "center",
     fontSize: 18,
     color: "#828AB0",
-    fontWeight: 700,
+    fontWeight: "700",
     marginBottom: 20
   }
 });
