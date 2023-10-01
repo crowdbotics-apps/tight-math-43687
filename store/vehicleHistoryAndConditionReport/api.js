9b8d5ba4a84090c441d0de06c609f86681a7c0cb
Crowdbotics <engineering@crowdbotics.com>
2023-10-01 14:07:14 +0000

    Update store.

---
+++ store/vehicleHistoryAndConditionReport/api.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+const vehicleHistoryAndConditionReport = axios.create({
+  baseURL: "http://api.carsxe.com",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function vehiclehistoryandconditionreport_get_specs_read(payload) {
+  return vehicleHistoryAndConditionReport.get(`/specs`, {
+    params: { key: payload.key, vin: payload.vin }
+  })
+}
+function vehiclehistoryandconditionreport_get_history_read(payload) {
+  return vehicleHistoryAndConditionReport.get(`/history`, {
+    params: { key: payload.key, vin: payload.vin }
+  })
+}
+function vehiclehistoryandconditionreport_get_marketvalue_read(payload) {
+  return vehicleHistoryAndConditionReport.get(`/marketvalue`, {
+    params: { key: payload.key, vin: payload.vin, format: payload.format }
+  })
+}
+export const apiService = {
+  vehiclehistoryandconditionreport_get_specs_read,
+  vehiclehistoryandconditionreport_get_history_read,
+  vehiclehistoryandconditionreport_get_marketvalue_read
+}
---
+++ store/vehicleHistoryAndConditionReport/vehiclehistoryandconditionreport_response_get_VehicleSpecifications.slice.js
@@ -0,0 +1,57 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const vehiclehistoryandconditionreport_get_specs_read = createAsyncThunk(
+  "vehiclehistoryandconditionreport_response_get_VehicleSpecifications/vehiclehistoryandconditionreport_get_specs_read",
+  async payload => {
+    const response = await apiService.vehiclehistoryandconditionreport_get_specs_read(
+      payload
+    )
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const vehiclehistoryandconditionreport_response_get_VehicleSpecificationsSlice = createSlice(
+  {
+    name: "vehiclehistoryandconditionreport_response_get_VehicleSpecifications",
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+      builder
+        .addCase(
+          vehiclehistoryandconditionreport_get_specs_read.pending,
+          (state, action) => {
+            if (state.api.loading === "idle") {
+              state.api.loading = "pending"
+            }
+          }
+        )
+        .addCase(
+          vehiclehistoryandconditionreport_get_specs_read.fulfilled,
+          (state, action) => {
+            if (state.api.loading === "pending") {
+              state.entities = [
+                ...state.entities.filter(
+                  record => record.id !== action.payload.id
+                ),
+                action.payload
+              ]
+              state.api.loading = "idle"
+            }
+          }
+        )
+        .addCase(
+          vehiclehistoryandconditionreport_get_specs_read.rejected,
+          (state, action) => {
+            if (state.api.loading === "pending") {
+              state.api.error = action.error
+              state.api.loading = "idle"
+            }
+          }
+        )
+    }
+  }
+)
+export default {
+  vehiclehistoryandconditionreport_get_specs_read,
+  slice: vehiclehistoryandconditionreport_response_get_VehicleSpecificationsSlice
+}
